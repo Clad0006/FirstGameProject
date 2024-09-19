4eb4ce59d79a99e124e5e5c632b4e6d5edd9885c
Valentin Cladel <valentin.cladel@gmail.com>
2024-09-19 22:06:09 +0200

    Directions et collisions

--- player.gd
+++ player.gd
@@ -1,12 +1,63 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("left_axis"):
-		var direction := Input.get_axis("ui_left","ui_right")
-		if direction:
-			velocity.x = direction * SPEED
-		else:
+const SPEED = 300.0
+const DASH_SPEED = 10.0
+const DOUBLETAP_DELAY = .15
+
+var doubletap_time = DOUBLETAP_DELAY
+var last_keycode = 0
+var doubletap = false
+var direction_anim = "front"
+
+func _ready() -> void:
+	anim.play("idle_front")
+
+func _process(delta):
+	doubletap_time -= delta
+	
+func _input(event):
+	if event is InputEventKey and event.is_released():
+		if last_keycode == event.keycode and doubletap_time >= 0:
+			doubletap=true
+			last_keycode = 0
+		else :
+			last_keycode = event.keycode
+	doubletap_time = DOUBLETAP_DELAY
+	
+func _physics_process(delta):
+	var direction_horizontal := Input.get_axis("ui_left","ui_right")
+	var direction_vertical := Input.get_axis("ui_up","ui_down")
+	if direction_horizontal:
+		velocity.x = direction_horizontal * SPEED
+		if velocity.x < 0 :
+			anim.play("walk_left")
+			direction_anim = "left"
+		else :
+			anim.play("walk_right")
+			direction_anim = "right"
+	else :
+		if doubletap :
+			velocity.x = move_toward(velocity.x*DASH_SPEED, 0, SPEED)
+		else :
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-		move_and_slide()
+			if !direction_vertical :
+				anim.play("idle_"+direction_anim)
+	if direction_vertical:
+		velocity.y = direction_vertical * SPEED
+		if velocity.y >0 :
+			anim.play("walk_front")
+			direction_anim = "front"
+		else :
+			anim.play("walk_back")
+			direction_anim = "back"
+	else :
+		if doubletap :
+			velocity.y = move_toward(velocity.y*DASH_SPEED, 0, SPEED)
+		else :
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+			if !direction_horizontal :
+				anim.play("idle_"+direction_anim)
+	move_and_slide()
+	doubletap = false
